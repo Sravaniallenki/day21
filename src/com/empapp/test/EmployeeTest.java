4e9dc678235032bedb4019dcca5ca7399b8d2135
sravani <allenkisravani996@gmail.com>
2020-05-16 23:12:13 +0530

    Refractored the EmployeeWageBulider

--- src/com/empapp/test/EmployeeTest.java
+++ src/com/empapp/test/EmployeeTest.java
@@ -1,14 +1,15 @@
 package com.empapp.test;
 import com.empapp.model.*;
 import com.empapp.service.*;
+import java.util.*;
 public class EmployeeTest
 {
     public static void main(String[] args)
     {
         System.out.println("WELCOME TO EMPLOYEE WAGE COMPUTATION");
         EmpWage obj = new EmployeeWageBuilder();
-        obj.addEmpWageCompany("HCL",30,10,5,24,120);
-        obj.addEmpWageCompany("AMAZON",20,8,4,20,100);
+        obj.addCompanyEmpWage("HCL",30,10,5,24,120);
+        obj.addCompanyEmpWage("AMAZON",20,8,4,20,100);
         obj.computeEmpWage();
     }
 }
